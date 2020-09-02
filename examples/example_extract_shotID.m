6fff92968aefa7496574c827561ae71a5abb62b0
Hao Zhang <albert.zhang.hit@gmail.com>
2020-09-01 22:43:17 -0700

    remove compiled results

--- examples/example_extract_shotID.m
+++ examples/example_extract_shotID.m
@@ -2,11 +2,10 @@
 
 %inFile = 'D:\SeismicDataSet\CNN\Northwest_AI\005Split\005_3A_group_dctl_8337_shotID_443676_450848.sgy';
 %outPrefix = 'D:\SeismicDataSet\CNN\Northwest_AI\TrainSet\005_dctl_8337';
-inFile = 'G:\Data_Field\Data_GeoEast\mz16_sy1_3d_sl10_tseq.segy';
-outPrefix = 'D:\SeismicDataSet\segy\mz16_sy1_3d_s110_tseq';
-leftShotID = 8134;
-rightShotID = 8106;
-maxTracesPerShot = 2010;
+inFile = 'E:\Datasets\SeismicData\Lab\P1007_NUCNS_AGORA2_SHOT_FLATST.SGY';
+outPrefix = 'E:\Projects\LabProject\VSP3D-master\3D_SHOT_2.segy';
+ShotID = [30321, 102116];
+
 tic
-shotExtract(inFile, outPrefix, leftShotID, rightShotID, maxTracesPerShot);
+shotExtract(inFile, outPrefix, ShotID);
 toc
